d00802a864d5b0d64cfd3405d77c3f0ead60df03
mktbsh <t93.hasebe@gmail.com>
2021-07-01 00:51:05 +0900

    add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
