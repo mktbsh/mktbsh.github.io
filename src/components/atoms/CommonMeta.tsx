5ec6335224b0d1633367b57a6e013a85732d39d7
mktbsh <t93.hasebe@gmail.com>
2021-07-07 23:54:15 +0900

    Update CommonMeta.tsx
    ファビコンの読み込みを追加

--- src/components/atoms/CommonMeta.tsx
+++ src/components/atoms/CommonMeta.tsx
@@ -1,23 +1,25 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 type MetaProps = {
-    title: string;
-    description: string;
-    image?: string;
-}
+  title: string;
+  description: string;
+  image?: string;
+};
 
-const CommonMeta: React.VFC<MetaProps> = ({ title, description = `mktbsh's github pages app.` }) => {
-    return (
-        <Head>
-            <title>{title} | mktbsh.github.io</title>
-            <meta property="description" content={description} />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta name="twitter:card" content="summary_large_image"/>
-        </Head>
-    )
-}
+const CommonMeta: React.VFC<MetaProps> = ({
+  title,
+  description = `mktbsh's github pages app.`,
+}) => {
+  return (
+    <Head>
+      <title>{title} | mktbsh.github.io</title>
+      <meta property="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
 
-export {
-    CommonMeta
-}
+export { CommonMeta };
