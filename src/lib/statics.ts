8666d5b35d483e1709962cc247b15d77841f01d4
mktbsh <t93.hasebe@gmail.com>
2021-07-07 23:54:47 +0900

    Update statics.ts
    JSONの配置場所を変更したのでパスも変更

--- src/lib/statics.ts
+++ src/lib/statics.ts
@@ -4,7 +4,7 @@ import path from "path";
 import type { Features } from "types/static/Features";
 
 export function getFeaturesJson(): Features {
-    const fullPath = path.join(`public`, `json`, `features.json`);
+    const fullPath = path.join(`configs`, `json`, `features.json`);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
 
     return JSON.parse(fileContents);
