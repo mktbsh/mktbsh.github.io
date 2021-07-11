65a78ddd9b1902126e7bea02ebb6526e0e589fe1
mktbsh <t93.hasebe@gmail.com>
2021-07-11 21:55:40 +0900

    [update] ポケモンデータの読み込み処理をつ以下

--- src/lib/statics.ts
+++ src/lib/statics.ts
@@ -2,9 +2,17 @@ import fs from "fs";
 import path from "path";
 
 import type { Features } from "types/static/Features";
+import { PokemonObject } from "types/static/Pokemon";
 
 export function getFeaturesJson(): Features {
-    const fullPath = path.join(`configs`, `json`, `features.json`);
+    const fullPath = path.join(`contents`, `json`, `features.json`);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    return JSON.parse(fileContents);
+}
+
+export function getKantoPokemonJson(): PokemonObject[] {
+    const fullPath = path.join(`contents`, `json`, `pokemon-kanto.json`);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
 
     return JSON.parse(fileContents);
