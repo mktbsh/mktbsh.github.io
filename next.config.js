d23195c2b12048e708675d5966d6fe9660722665
mktbsh <t93.hasebe@gmail.com>
2021-07-11 21:54:56 +0900

    [update] next.config.js: i18n

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ module.exports = {
     poweredByHeader: false,
     reactStrictMode: true,
     trailingSlash: true,
+    i18n: {
+        locales: ['ja', 'en', 'unown'],
+        defaultLocale: 'ja'
+    },
     env: {
         resumeUrl: 'https://raw.githubusercontent.com/mktbsh/resume/master/docs/README.md',
     },
