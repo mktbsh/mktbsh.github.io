1fbb56ce3bab76640f6b780f63797a16b37d52b1
mktbsh <t93.hasebe@gmail.com>
2022-05-13 21:19:53 +0900

    [delete] 未着手のページをすべて削除

--- src/lib/statics.ts
+++ src/lib/statics.ts
@@ -2,18 +2,10 @@ import fs from "fs";
 import path from "path";
 
 import type { Features } from "types/static/Features";
-import { PokemonObject } from "types/static/Pokemon";
 
 export function getFeaturesJson(): Features {
-    const fullPath = path.join(`contents`, `json`, `features.json`);
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
+  const fullPath = path.join(`contents`, `json`, `features.json`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
 
-    return JSON.parse(fileContents);
-}
-
-export function getKantoPokemonJson(): PokemonObject[] {
-    const fullPath = path.join(`contents`, `json`, `pokemon-kanto.json`);
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
-
-    return JSON.parse(fileContents);
+  return JSON.parse(fileContents);
 }
