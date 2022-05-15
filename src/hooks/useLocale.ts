0f5a2060bc9140a1485d87fec7b0771c1be72976
mktbsh <t93.hasebe@gmail.com>
2022-05-15 21:35:41 +0900

    Delete useLocale.ts

--- src/hooks/useLocale.ts
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router'
-
-export const useRocale = () => {
-    const { locale } = useRouter();
-
-    const t = locale === 'ja' ? 'ja' : 'en';
-
-    return { locale, t };
-}
